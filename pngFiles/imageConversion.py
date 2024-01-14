483358bf8c65475215e73179e2c18a6a50f44e2e
Matt Posey <Mattjposey@gmail.com>
2024-01-13 21:02:36 -0800

    first commit

---
+++ pngFiles/imageConversion.py
@@ -0,0 +1,17 @@
+from PIL import Image
+import os
+
+def convert_png_to_jpg(input_folder, output_folder):
+    for filename in os.listdir(input_folder):
+        if filename.endswith(".png"):
+            png_path = os.path.join(input_folder, filename)
+            jpg_path = os.path.join(output_folder, os.path.splitext(filename)[0] + ".jpg")
+
+            img = Image.open(png_path)
+            img.convert("RGB").save(jpg_path, "JPEG")
+            print(f"Converted {filename} to {os.path.basename(jpg_path)}")
+
+# Example usage
+input_folder = "path/to/png_files"
+output_folder = "path/to/output_jpg_files"
+convert_png_to_jpg(input_folder, output_folder)
